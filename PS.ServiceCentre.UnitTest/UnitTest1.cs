5e06f2d461721452f7076d507d18147eca061db9
Raihan <raihan.iqbal@gmail.com>
2016-09-14 16:54:06 +1000

    Some clean up before moving to new branch

--- PS.ServiceCentre.UnitTest/UnitTest1.cs
+++ PS.ServiceCentre.UnitTest/UnitTest1.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PS.Puzzle03.Models;
+using System.Web;
 
 namespace PS.ServiceCentre.UnitTest
 {
     [TestClass]
     public class UnitTest1
     {
+        [TestInitialize]
+        public void TestInit()
+        {
+            // TO DO: Set up fake HttpContext for PhysicalApplicationPath to work
+
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
