761f187d4a3e02a047a73f62b18129299d5bb30f
Raihan Iqbal <raihan.iqbal@gmail.com>
2016-09-15 10:08:45 +1000

    Created directive skeleton and added other UI dependencies

---
+++ PS.Puzzle03/Scripts/app/pasServiceCentreApp.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('pasServiceCentreApp', []);
+
+    app.directive('pasServiceCentreLocator', pasServiceCentreLocator);
+
+    function pasServiceCentreLocator() {
+        return {
+            replace: true,
+            templateUrl: '/Scripts/app/templates/pasServiceCentreLocatorTemplate.html',
+            controller: pasServiceCentreLocatorCtrl,
+            controllerAs: 'pasCtrl'
+        };
+    }
+
+    pasServiceCentreLocatorCtrl.$inject = ['$scope'];
+
+    function pasServiceCentreLocatorCtrl($scope) {
+
+    }
+
+})();
