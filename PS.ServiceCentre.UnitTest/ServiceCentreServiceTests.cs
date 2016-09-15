a9b2380809bd2ea07fca5149d90576c3d5b0bc5d
Raihan Iqbal <raihan.iqbal@gmail.com>
2016-09-15 18:20:35 +1000

    Added unit test for service

---
+++ PS.ServiceCentre.UnitTest/ServiceCentreServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PS.Puzzle03.Models;
+using System.Web;
+using PS.Puzzle03.Domain;
+using PS.UnitTest.Mocks;
+
+namespace PS.UnitTest.Tests
+{
+    [TestClass]
+    public class ServiceCentreServiceTests
+    {
+        IServiceCentreService _service;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _service = new ServiceCentreService(new MockRepository());
+        }
+
+        [TestMethod]
+        public void Can_Get_Nearby_Service_Centres()
+        {
+            Assert.IsNotNull(_service.GetNearby(new MapLocation() { }));
+        }
+    }
+}
